127bd9f1b3b4e89a96595bfce02304ee602dbcf4
Daniel Grönlund <just.danne@gmail.com>
2024-04-03 11:24:49 +0200

    Add Swift Package Manager definition

---
+++ Package.swift
@@ -0,0 +1,40 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+  name: "opennurbs",
+  products: [
+    .library(
+      name: "opennurbs",
+      targets: ["opennurbs"]),
+  ],
+  targets: [
+    .target(
+      name: "opennurbs",
+      path: ".",
+      exclude: [
+        "android_uuid/",
+        "example_brep/",
+        "example_convert/",
+        "example_files/",
+        "example_gl/",
+        "example_read/",
+        "example_roundtrip/",
+        "example_test/",
+        "example_userdata/",
+        "example_write/",
+        "freetype263/",
+        "zlib/",
+        "opennurbs_unicode_cp949.cpp",
+        "opennurbs_unicode_cp932.cpp"
+      ],
+      sources: ["."],
+      cxxSettings: [
+        .unsafeFlags(["-Wno-elaborated-enum-class"]),
+        .define("ON_COMPILING_OPENNURBS"),
+      ]
+    ),
+  ],
+  cxxLanguageStandard: .cxx17
+)
